10b2aa4533730d511d332aa7ae81008f832832ad
Kevin Hwang <k3vinhwang@gmail.com>
2015-06-14 20:14:45 -0700

    Add simple dragon which falls and can jump

--- js/index.js
+++ js/index.js
@@ -2,11 +2,36 @@ var game = new Phaser.Game(400, 490, Phaser.AUTO, "gameDiv");
 
 var mainState = {
   preload: function () {
+    game.stage.backgroundColor = "#71c5cf";
+    game.load.image("dragon", "assets/dragon.png");
   },
+
   create: function () {
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+
+    this.dragon = this.game.add.sprite(100, 245, "dragon");
+
+    game.physics.arcade.enable(this.dragon);
+    this.dragon.body.gravity.y = 1000;
+
+    var spaceKey = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+    spaceKey.onDown.add(this.jump, this);
   },
+
   update: function () {
+    if (!this.dragon.inWorld) {
+      this.restartGame();
+    }
+  },
+
+  jump: function () {
+    this.dragon.body.velocity.y = - 350;
+  },
+
+  restartGame: function () {
+    game.state.start("main");
   }
+
 };
 
 game.state.add("main", mainState);
